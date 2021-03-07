043e4b6615364a3e5bdcd05fb37549aa0f9ca01f
99003510 <rajathpd.pd@ltts.com>
2021-03-08 00:50:45 +0530

    Update header.h

--- Inc/header.h
+++ Inc/header.h
@@ -2,8 +2,9 @@
 #include<string.h>
 #include<stdlib.h>
 
+ 
  //int display(struct box *b2,int x);
-//int insert(struct box *b2,int increment,int update_id,float update_length,float update_breadth,float update_height,float update_weight,char update_color[20]);
+//int insert(struct box *b2,int increment,int update_id,float update_length,float update_breadth,float update_height,float 
  //int search(struct box *b2,int x,int search_id);
  //float max_height(struct box *ptr);
  struct box{
